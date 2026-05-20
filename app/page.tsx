7821c56e939e24562fa4e59acee57b717fee55fb
Pratik Kamble <pratik.kamble@dynamisch.co>
2026-05-20 13:00:51 +0530

    added font setup and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div className="">
-            <h1>Welcome to Next.js!</h1>
-        </div>
-    );
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-light-500">Welcome to Next.js!</h1>
+      <h1 className="text-light-500 font-space-grotesk">Welcome to Next.js!</h1>
+    </div>
+  );
 }
