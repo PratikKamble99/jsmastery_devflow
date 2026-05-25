6dd4c33a13dce68ce9c5abe78347053f36e17aa4
Pratik Kamble <pratik.kamble@dynamisch.co>
2026-05-25 14:07:06 +0530

    Implemented local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,75 @@
-import LogOutButton from "@/components/shared/button/LogOutButton";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import Link from "next/link";
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  console.log("Session:", session);
-  return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="text-light-500">Welcome to Next.js!</h1>
-      <LogOutButton />
-    </div>
-  );
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+
+interface Props {
+  searchParams: Promise<{ [key: string]: string }>;
 }
+
+const Home = async ({ searchParams }: Props) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) => {
+    return question.title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.HOME}
+          imgSrc="icons/search.svg"
+          placeholder="Search questions..."
+          iconPosition="left"
+        />
+      </section>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <p key={question._id}>{question.title}</p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import Image from "next/image";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/url";
+import { useRouter } from "next/navigation";
+import router from "next/router";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  iconPosition: "left" | "right";
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, iconPosition, otherClasses }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  const [searchQuery, setSearchQuery] = useState(query || "");
+
+  useEffect(() => {
+    const debouncedTimer = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({ params: searchParams.toString(), key: "query", value: searchQuery });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({ params: searchParams.toString(), keysToRemove: ["query"] });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(debouncedTimer);
+  }, [searchQuery, route, pathname, router]);
+
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === "left" && <Image src={imgSrc} alt="search" width={24} height={24} className="cursor-pointer" />}
+
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-hidden"
+      />
+      {iconPosition === "right" && (
+        <Image src={imgSrc} alt="search" width={15} height={15} className="cursor-pointer" />
+      )}
+    </div>
+  );
+};
+
+export default LocalSearch;
